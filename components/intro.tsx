9b59e80d11d6017285d382cec2083d9395314893
florinbighiu <120215264+florinbighiu@users.noreply.github.com>
2024-02-23 08:55:50 +0200

    Added social links to intro component

--- components/intro.tsx
+++ components/intro.tsx
@@ -78,13 +78,13 @@ export default function Intro() {
         <div className="flex flex-row gap-2">
           <a
             className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-            href="https://linkedin.com"
+            href="https://www.linkedin.com/in/florin-bighiu/"
             target="_blank">
             <BsLinkedin />
           </a>
           <a
             className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-            href="https://github.com"
+            href="https://github.com/florinbighiu"
             target="_blank">
             <FaGithubSquare />
           </a>
