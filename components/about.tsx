5752e2fcdaacf02d12b531134dec2893ea90273f
florinbighiu <120215264+florinbighiu@users.noreply.github.com>
2024-02-27 20:49:03 +0200

    About page layout fix

--- components/about.tsx
+++ components/about.tsx
@@ -15,20 +15,22 @@ export default function About() {
   return (
     <motion.section
       ref={ref}
-      className="mb-28 w-full text-center leading-8 sm:mb-40 scroll-mt-28"
+      className="mb-28 mt-3 w-full text-center leading-8 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
       id="about">
       <SectionHeading>About me</SectionHeading>
-      <div className="flex flex-col lg:flex-row gap-[3rem] p-8">
-        <Image
-          src={profileImg}
-          alt="florin"
-          className="lg:w-1/3 h-full rounded-lg object-cover border-[0.2rem] dark:border-white/65 border-slate-300/65 shadow-xl"
-        />
-        <div className="my-5">
-          <p className="m-3 text-xl text-start tracking-tighter">
+      <div className="flex flex-col lg:flex-row gap-[3rem]">
+        <div className="flex items-start lg:items-center justify-center lg:justify-end">
+          <Image
+            src={profileImg}
+            alt="florin"
+            className="w-2/3 lg:w-2/3 h-[30rem] transition-all hover:translate-x-2 hover:translate-y-2 cursor-pointer rounded-lg object-cover border-[0.2rem] dark:border-white/65 border-slate-300/65 shadow-xl"
+          />
+        </div>
+        <div className="lg:mx-6 mx-2 my-4 flex-1">
+          <p className="m-2 text-xl text-start tracking-tighter">
             I am primarily a patient individual who excels in high-pressure environments and
             consistently embraces an optimistic perspective. My ability to remain composed and
             positive in challenging situations enables me to approach tasks and projects with a
@@ -37,36 +39,30 @@ export default function About() {
             effectively to a team's success and deliver results even in the face of adversity.
           </p>
           <section className="py-8">
-            <div className="max-w-2xl m-3">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                <div>
-                  <ul className="space-y-4 font-semibold">
-                    <li className="flex items-center">
-                      <FaEnvelope className="mr-2" />
-                      <span>florin.bighiu1@gmail.com</span>
-                    </li>
-                    <li className="flex items-center">
-                      <FaPhone className="mr-2" />
-                      <span>+40 748 964 313</span>
-                    </li>
-                    <li className="flex items-center">
-                      <FaMapMarkerAlt className="mr-2" />
-                      <span>Iasi, Romania</span>
-                    </li>
-                  </ul>
-                </div>
-                <div>
-                  <ul className="space-y-4 font-semibold">
-                    <li className="flex items-center">
-                      <FaGithub className="mr-2" />
-                      <a href="https://github.com/florinbighiu">florinbighiu</a>
-                    </li>
-                    <li className="flex items-center">
-                      <FaLinkedin className="mr-2" />
-                      <a href="https://www.linkedin.com/in/florin-bighiu/">florin-bighiu</a>
-                    </li>
-                  </ul>
-                </div>
+            <div className="max-w-2xl m-2">
+              <div className="">
+                <ul className="grid sm:grid-cols-2 gap-y-3 gap-x-5 font-semibold">
+                  <li className="flex items-center">
+                    <FaPhone className="mr-2" />
+                    <span>+40 748 964 313</span>
+                  </li>
+                  <li className="flex items-center">
+                    <FaMapMarkerAlt className="mr-2" />
+                    <span>Iasi, Romania</span>
+                  </li>
+                  <li className="flex items-center">
+                    <FaLinkedin className="mr-2" />
+                    <a href="https://www.linkedin.com/in/florin-bighiu/">florin-bighiu</a>
+                  </li>
+                  <li className="flex items-center">
+                    <FaGithub className="mr-2" />
+                    <a href="https://github.com/florinbighiu">florinbighiu</a>
+                  </li>
+                  <li className="flex items-center">
+                    <FaEnvelope className="mr-2" />
+                    <span>florin.bighiu1@gmail.com</span>
+                  </li>
+                </ul>
               </div>
             </div>
           </section>
