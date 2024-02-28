7ec66eaef2058bf42abe00665fec7628db77c303
florinbighiu <120215264+florinbighiu@users.noreply.github.com>
2024-02-28 20:28:56 +0200

    About page component refactor

--- components/about.tsx
+++ components/about.tsx
@@ -21,51 +21,47 @@ export default function About() {
       transition={{ delay: 0.175 }}
       id="about">
       <SectionHeading>About me</SectionHeading>
-      <div className="flex flex-col lg:flex-row gap-[3rem]">
-        <div className="flex items-start lg:items-center justify-center lg:justify-end">
+      <div className="container mx-auto px-4 py-16">
+        <div className="flex flex-col lg:flex-row md:space-x-8">
           <Image
+            className="transition-all hover:translate-x-2 hover:translate-y-2 cursor-pointer rounded-lg object-cover border-[0.2rem] dark:border-white/65 border-slate-300/65 shadow-xl w-full lg:w-1/2 h-96"
             src={profileImg}
-            alt="florin"
-            className="w-2/3 lg:w-2/3 h-[30rem] transition-all hover:translate-x-2 hover:translate-y-2 cursor-pointer rounded-lg object-cover border-[0.2rem] dark:border-white/65 border-slate-300/65 shadow-xl"
+            alt="Florin's picture"
           />
-        </div>
-        <div className="lg:mx-6 mx-2 my-4 flex-1">
-          <p className="m-2 text-xl text-start tracking-tighter">
-            I am primarily a patient individual who excels in high-pressure environments and
-            consistently embraces an optimistic perspective. My ability to remain composed and
-            positive in challenging situations enables me to approach tasks and projects with a
-            level-headed attitude and a focus on finding solutions. I thrive in situations that
-            demand resilience, adaptability, and a constructive outlook, allowing me to contribute
-            effectively to a team's success and deliver results even in the face of adversity.
-          </p>
-          <section className="py-8">
-            <div className="max-w-2xl m-2">
-              <div className="">
-                <ul className="grid sm:grid-cols-2 gap-y-3 gap-x-5 font-semibold">
-                  <li className="flex items-center">
-                    <FaPhone className="mr-2" />
-                    <span>+40 748 964 313</span>
-                  </li>
-                  <li className="flex items-center">
-                    <FaMapMarkerAlt className="mr-2" />
-                    <span>Iasi, Romania</span>
-                  </li>
-                  <li className="flex items-center">
-                    <FaLinkedin className="mr-2" />
-                    <a href="https://www.linkedin.com/in/florin-bighiu/">florin-bighiu</a>
-                  </li>
-                  <li className="flex items-center">
-                    <FaGithub className="mr-2" />
-                    <a href="https://github.com/florinbighiu">florinbighiu</a>
-                  </li>
-                  <li className="flex items-center">
-                    <FaEnvelope className="mr-2" />
-                    <span>florin.bighiu1@gmail.com</span>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </section>
+          <div className="flex flex-col space-y-4 mt-6 lg:mt-0 xl:mt-3">
+            <p className="text-gray-700 dark:text-gray-100 text-start leading-loose">
+              I am primarily a patient individual who excels in high-pressure environments and
+              consistently embraces an optimistic perspective. My ability to remain composed and
+              positive in challenging situations enables me to approach tasks and projects with a
+              level-headed attitude and a focus on finding solutions. I thrive in situations that
+              demand resilience, adaptability, and a constructive outlook, allowing me to contribute
+              effectively to a team's success and deliver results even in the face of adversity.
+            </p>
+            <ul className="grid sm:grid-cols-2 gap-y-3 gap-x-5">
+              <li className="flex items-center">
+                <FaPhone className="mr-2" />
+                <span>
+                  <a href="tel:+40748964313">+40 748 964 313</a>
+                </span>
+              </li>
+              <li className="flex items-center">
+                <FaMapMarkerAlt className="mr-2" />
+                <span>Iasi, Romania</span>
+              </li>
+              <li className="flex items-center">
+                <FaLinkedin className="mr-2" />
+                <a href="https://www.linkedin.com/in/florin-bighiu/">florin-bighiu</a>
+              </li>
+              <li className="flex items-center">
+                <FaGithub className="mr-2" />
+                <a href="https://github.com/florinbighiu">florinbighiu</a>
+              </li>
+              <li className="flex items-center">
+                <FaEnvelope className="mr-2" />
+                <span>florin.bighiu1@gmail.com</span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </motion.section>
