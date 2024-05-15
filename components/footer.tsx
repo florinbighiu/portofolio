096575c96c5fa2cfa867984264c379a0050cab36
Florin Bighiu <florin.bighiu1@gmail.com>
2024-05-15 13:21:00 +0300

    Changed footer text

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="my-5 px-4 pt-4 border-t border-gray-800/10 dark:border-gray-100/10 text-center text-gray-500">
-      <small className="my-2 block text-xs">&copy; 2024 Florin Bighiu. All rights reserved.</small>
+      <small className="my-2 block text-xs">&copy; 2024 Florin Bighiu</small>
     </footer>
   );
 }
