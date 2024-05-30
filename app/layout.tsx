eb0e9b653dd39fbc3efa61f68507b4548678ba18
Florin <florin.bighiu1@gmail.com>
2024-05-30 10:27:49 +0300

    Theme switch fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`${poppins.className} bg-gradient-to-r from-gray-50 to-violet-100 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
+        className={`${poppins.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
             <Header />
