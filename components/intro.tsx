1a4af9e262c85f49ee1c2d93366c6542d25736e7
Florin <florin.bighiu1@gmail.com>
2024-08-24 12:39:42 +0300

    CV path fix

--- components/intro.tsx
+++ components/intro.tsx
@@ -71,7 +71,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Florin-Bighiu_-_Full-Stack_Developer.pdf"
+          href="/Florin-Bighiu_-_Full_Stack_Developer.pdf"
           download>
           Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
